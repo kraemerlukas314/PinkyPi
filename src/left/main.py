7731a722481372e9ec6d2fa13e36821ff47451d5
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2024-06-29 21:03:13 +0200

    communication

--- src/left/main.py
+++ src/left/main.py
@@ -3,32 +3,31 @@
 import board
 import digitalio
 import time
+import math
+clock_pin = digitalio.DigitalInOut(board.GP4)
+clock_pin.direction = digitalio.Direction.OUTPUT
 
-clock = digitalio.DigitalInOut(board.GP4)
-clock.direction = digitalio.Direction.OUTPUT
-
-data = digitalio.DigitalInOut(board.GP5)
-data.direction = digitalio.Direction.OUTPUT
+data_pin = digitalio.DigitalInOut(board.GP5)
+data_pin.direction = digitalio.Direction.OUTPUT
 
 trigger_pin = digitalio.DigitalInOut(board.GP2)
 trigger_pin.direction = digitalio.Direction.INPUT
 trigger_pin.pull = digitalio.Pull.UP
 
-led_pin = digitalio.DigitalInOut(board.GP25)
-led_pin.direction = digitalio.Direction.OUTPUT
-
-freq = 1000 # communication frequency in hz
+freq = 1 # communication frequency in bits/second
 
 # function to send any number to the other Pico
 def send_data(number, freq):
-    for i in range(number):
+    for i in range(math.ceil(math.log(8 + 1) / math.log(2))): # loop through every bit
         bit = (number >> i) & 0x01
-        clock.value = True
-        data.value = bit
+        clock_pin.value = True
+        data_pin.value = bit
         time.sleep(1/freq)
-        clock.value = False
+        clock_pin.value = False
       
 while True:
-    while trigger_pin.value:
-        pass
-    send_data(5, freq)
+    for i in range(100):
+        while trigger_pin.value:
+            pass
+        send_data(i, freq)
+        time.sleep(0.5)
--- src/right/main.py
+++ src/right/main.py
@@ -4,19 +4,37 @@ import board
 import digitalio
 import time
 
-data = digitalio.DigitalInOut(board.GP5)
-data.direction = digitalio.Direction.INPUT
-clock = digitalio.DigitalInOut(board.GP4)
-clock.direction = digitalio.Direction.INPUT
+clock_pin = digitalio.DigitalInOut(board.GP4)
+clock_pin.direction = digitalio.Direction.INPUT
+
+data_pin = digitalio.DigitalInOut(board.GP5)
+data_pin.direction = digitalio.Direction.INPUT
+
+freq = 10 # communication frequency in bits/second
+
+def receive_data(freq):
+     # if there is no new clock cycle for 1.5 estimated cycles, communication is probably over
+    TIMEOUT = 1.5 * (1 / freq) * 10**9  # in nanoseconds
+    communication_end = False
+    bits = []
+    while not communication_end:
+             # wait for half clock cycle to let data line stabilze
+            time.sleep(1/freq * 0.5)
+            bits.insert(0, data_pin.value)
+            print(bits)
+            # wait for HL edge (falling)
+            while clock_pin.value:
+                pass
+            hl_edge_timestamp = time.monotonic_ns()
+            
+            # wait for LH edge (rising) or timeout
+            while not clock_pin.value and not communication_end:
+                time_passed_since_LH_edge = (time.monotonic_ns() - hl_edge_timestamp) # in ns
+                if time_passed_since_LH_edge > TIMEOUT:
+                    communication_end = True
+    binary_str = ''.join('1' if bit else '0' for bit in bits)
+    return int(binary_str, 2)
 
 while True:
-    if (data.value):
-        if (clock.value):
-            print(1, 1)
-        else:
-            print(0, 1)
-    else:
-        if (clock.value):
-            print(1, 0)
-        else:
-            print(0, 0)
+    if clock_pin.value:
+        print(receive_data(freq))
