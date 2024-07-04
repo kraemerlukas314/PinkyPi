b393565fa3e96efd96cdbe23beb2333700c1f7af
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2024-07-04 13:50:57 +0200

    not working

--- src/left/main.py
+++ src/left/main.py
@@ -1,58 +1,35 @@
-### CODE FOR LEFT HALF ###
-
 import board
 import digitalio
 import time
-import math
-clock_pin = digitalio.DigitalInOut(board.GP4)
-clock_pin.direction = digitalio.Direction.OUTPUT
 
-data_pin = digitalio.DigitalInOut(board.GP5)
-data_pin.direction = digitalio.Direction.OUTPUT
+# Setup for GPIO5 as output
+gpio5 = digitalio.DigitalInOut(board.GP5)
+gpio5.direction = digitalio.Direction.OUTPUT
 
-led_pin = digitalio.DigitalInOut(board.GP25)
-led_pin.direction = digitalio.Direction.OUTPUT
-
-trigger_pin = digitalio.DigitalInOut(board.GP2)
-trigger_pin.direction = digitalio.Direction.INPUT
-trigger_pin.pull = digitalio.Pull.UP
-
-freq = 1 # communication frequency in bits/second
-
-# function to send any number to the other Pico
-def send_data(number, freq):
-    for i in range(required_bits(number)): # loop through every bit
-        bit = (number >> i) & 0x01
-        print(bit)
-        clock_pin.value = True
-        led_pin.value = True
-        data_pin.value = bit
-        time.sleep(1/freq)
-        clock_pin.value = False
-        led_pin.value = False
-    print("---")
-
-# returns number of required bits to send number
-# number must be >= 0
-def required_bits(number):
-    if number < 2:
-        return 1
-    else:
-        return math.ceil(math.log(number + 1) / math.log(2))
+def send_number(number):
+    """
+    Send a number by toggling the GPIO5 pin.
+    :param number: Number between 0-25
+    """
+    assert 0 <= number <= 25, "Number must be between 0 and 25"
+    
+    # Pull GPIO5 high to signal start of transmission
+    gpio5.value = True
+    time.sleep(0.01)  # Ensure the signal is high for a short duration
+    
+    # Send the number by toggling the GPIO5 pin
+    gpio5.value = False
+    for _ in range(number):
+        gpio5.value = True
+        time.sleep(0.0005)  # 1000 Hz frequency, half period
+        gpio5.value = False
+        time.sleep(0.0005)  # 1000 Hz frequency, half period
+    
+    # End of transmission
+    gpio5.value = False
 
 while True:
-    clock_pin.value = 1
-    data_pin.value = 0
-    led_pin.value = 1
-    time.sleep(5)
-    clock_pin.value = 0
-    data_pin.value = 1
-    led_pin.value = 0
-    time.sleep(5)
-        
-while True:
-    for i in range(100):
-        while trigger_pin.value:
-            pass
-        send_data(i, freq)
-        time.sleep(0.5)
+    for i in range(5, 25):
+        print("sending:", i)
+        send_number(i)
+        time.sleep(5)
--- src/right/main_right.py
+++ src/right/main_right.py
@@ -1,48 +1,33 @@
-### CODE FOR RIGHT HALF ###
-
 import board
 import digitalio
 import time
 
-clock_pin = digitalio.DigitalInOut(board.GP4)
-clock_pin.direction = digitalio.Direction.INPUT
+# Setup for GPIO5 as input
+gpio5 = digitalio.DigitalInOut(board.GP5)
+gpio5.direction = digitalio.Direction.INPUT
 
-data_pin = digitalio.DigitalInOut(board.GP5)
-data_pin.direction = digitalio.Direction.INPUT
+def received_number():
+    """
+    Receives the number sent by the left Pico.
+    :return: Received number
+    """
+    # Wait for GPIO5 to go high
+    while not gpio5.value:
+        time.sleep(0.001)
+    
+    # Start counting the toggles
+    count = 0
+    start_time = time.time()
+    while time.time() - start_time < 0.05:  # Monitor for 50 milliseconds
+        if gpio5.value:
+            count += 1
+            while gpio5.value:
+                pass  # Wait for the pin to go low
+    
+    return count
 
-freq = 1 # communication frequency in bits/second
-"""
-def receive_data(freq):
-     # if there is no new clock cycle for 1.5 estimated cycles, communication is probably over
-    TIMEOUT = 1.5 * (1 / freq) * 10**9  # in nanoseconds
-    communication_end = False
-    bits = []
-    while not communication_end:
-             # wait for half clock cycle to let data line stabilze
-            time.sleep(1/freq * 0.5)
-            bits.insert(0, data_pin.value)
-            print(bits)
-            # wait for HL edge (falling)
-            while clock_pin.value:
-                pass
-            hl_edge_timestamp = time.monotonic_ns()
-            
-            # wait for LH edge (rising) or timeout
-            while not clock_pin.value and not communication_end:
-                time_passed_since_LH_edge = (time.monotonic_ns() - hl_edge_timestamp) # in ns
-                if time_passed_since_LH_edge > TIMEOUT:
-                    communication_end = True
-    binary_str = ''.join('1' if bit else '0' for bit in bits)
-    return int(binary_str, 2)
-"""
+# Example usage: receive number
 while True:
-    if clock_pin.value:
-        clock = 1
-    else:
-        clock = 0
-    if data_pin.value:
-        data = 1
-    else:
-        data = 0
-    print(clock, data)
-    time.sleep(0.1)
+    if gpio5.value:
+        received_num = received_number()
+        print("Received number:", received_num)
